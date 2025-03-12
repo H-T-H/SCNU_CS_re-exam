e02d3e5e7a127f30b469d2eae1e5d33244a1d889
H-T-H <ouhthuo@gmail.com>
2025-03-12 17:31:08 +0800

    update

---
+++ 3.12/leetcode_125.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+        bool isPalindrome(string s) {
+            int left = 0, right = s.size()-1;
+            while(left < right){
+                while(!isalnum(s[left])&&left < right){
+                    left++;
+                }
+                while(!isalnum(s[right])&&right > left){
+                    right--;
+                }
+                if(tolower(s[left])!=tolower(s[right])){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        };
+};
